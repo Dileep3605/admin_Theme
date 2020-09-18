a2a778a49717057dc51693b87b7b4f9a305ebe4c
Dileep3605 <di.singh1992@gmail.com>
2020-09-18 10:09:51 +0530

    Added show error common component

---
+++ src/app/shared/components/show-error/show-error.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-show-error',
+  templateUrl: './show-error.component.html',
+  styleUrls: ['./show-error.component.scss'],
+})
+export class ShowErrorComponent implements OnInit {
+  @Input() ctrl: FormControl;
+
+  ERROR_MESSAGE = {
+    required: () => `This field is required`,
+    minlength: par => `Min ${par.requiredLength} chars is required`,
+    maxlength: par => `Max ${par.requiredLength} chars is required`,
+    min: par => `Min value ${par.min} is required`,
+    max: par => `Max value ${par.max} is required`,
+    pattern: () => `Enter only number`,
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  shouldShowErrors(): boolean {
+    console.log(this.ctrl.errors);
+    return this.ctrl && this.ctrl.errors && this.ctrl.touched;
+  }
+
+  listOfErrors(): string[] {
+    return Object.keys(this.ctrl.errors).map(err =>
+      this.ERROR_MESSAGE[err](this.ctrl.getError(err))
+    );
+  }
+}
